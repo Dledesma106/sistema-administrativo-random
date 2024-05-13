1059017a49bace0400a9e54600e2ac4c7c095b5e
Lypher <acosta.enzo@hotmail.com>
2024-05-13 13:14:20 -0300

    a

--- src/components/Tables/ClientBranchesTable/ClientBranchesTable.tsx
+++ src/components/Tables/ClientBranchesTable/ClientBranchesTable.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { type ChangeEvent, useState } from 'react';
+import { type ChangeEvent, useState, useEffect } from 'react';
 
 import Item from './Item';
-
-import Filter from '@/components/Filter';
 import {
     Table,
     TableBody,
@@ -12,6 +10,11 @@ import {
     TableRow,
 } from '@/components/ui/table';
 import { ValidClientViewProps } from '@/pages/tech-admin/clients/[clientId]/branches';
+import { TasksDataTableToolbar } from '@/modules/TasksDataTable/TableToolbar';
+import { ColumnFiltersState, SortingState, getCoreRowModel, getFacetedRowModel, getFilteredRowModel,getFacetedUniqueValues as globalGetFacetedUniqueValues, getPaginationRowModel, getSortedRowModel, useReactTable } from '@tanstack/react-table';
+import { BranchesQuery } from '@/api/graphql';
+import { useBranchesListQuery } from '@/modules/BranchesDataTable/queries';
+import { useBranchesTableColumns } from '@/modules/BranchesDataTable/columns';
 
 export type BranchTableProps = ValidClientViewProps;
 
@@ -24,8 +27,76 @@ export default function BranchTable({
     const [tableBranches, setTableBranches] = useState(branches);
 
     const [type, setType] = useState<string>('');
-    const [entities, setEntities] = useState<any[]>([] as any[]);
-    const filterTypes = ['Localidad', 'Provincia', 'Empresa'];
+
+    type TableItem = BranchesQuery['branches'][0];
+
+    const [sorting, setSorting] = useState<SortingState>([]);
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+
+    const branchesQuery = useBranchesListQuery({
+        business: null,
+        city: null,
+        client: null,
+        location: null,
+      
+    });
+
+    const [branches, setBranches] = useState(branchesQuery.data?.branches);
+
+    const columns = useBranchesTableColumns();
+
+    useEffect(() => {
+        setBranches(branchesQuery.data?.tasks);
+    }, [branchesQuery.data?.branches]);
+
+    const table = useReactTable<TableItem>({
+        data: branches || [],
+        columns: columns ,
+        onSortingChange: setSorting,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getFilteredRowModel: getFilteredRowModel(),
+        getFacetedRowModel: getFacetedRowModel(),
+        getFacetedUniqueValues: (table, columnId) => {
+            if (columnId === 'assigned') {
+                return () => {
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    const uniqueValuesMap = new Map<any, number>();
+                    const rows = table.getCoreRowModel().rows;
+
+                    rows.forEach((row) => {
+                        const assigned: TableItem['assigned'] = row.getValue(columnId);
+
+                        for (const user of assigned) {
+                            const valueInMap = uniqueValuesMap.get(user.id);
+
+                            if (typeof valueInMap !== 'undefined') {
+                                uniqueValuesMap.set(user.id, valueInMap + 1);
+                            } else {
+                                uniqueValuesMap.set(user.id, 1);
+                            }
+                        }
+                    });
+
+                    return uniqueValuesMap;
+                };
+            }
+
+            return globalGetFacetedUniqueValues<TableItem>()(table, columnId);
+        },
+        state: {
+            columnVisibility: {
+                branch: false,
+                business: false,
+                client: false,
+            },
+            sorting,
+            columnFilters,
+        },
+    });
+
 
     function selectEntity(e: ChangeEvent<HTMLSelectElement>): void {
         const { value } = e.target;
@@ -53,30 +124,7 @@ export default function BranchTable({
         }
     }
 
-    function selectType(e: ChangeEvent<HTMLSelectElement>): void {
-        const { value } = e.target;
-
-        setType(value);
-        switch (value) {
-            case 'Localidad':
-                setEntities(cities);
-                break;
-            case 'Provincia':
-                setEntities(provinces);
-                break;
-            case 'Empresa':
-                setEntities(businesses);
-                break;
-            default:
-                break;
-        }
-    }
-
-    function clearFilter(): void {
-        setType('');
-        setEntities([] as any[]);
-        setTableBranches(branches);
-    }
+    
 
     const deleteBranch = (id: string): void => {
         setTableBranches((prev) => {
@@ -86,13 +134,7 @@ export default function BranchTable({
 
     return (
         <div className="rounded-none">
-            <Filter
-                types={filterTypes}
-                entities={entities}
-                selectType={selectType}
-                selectEntity={selectEntity}
-                clearFilter={clearFilter}
-            />
+           <TasksDataTableToolbar table={table} {...props} />
 
             <Table>
                 <TableHeader>
