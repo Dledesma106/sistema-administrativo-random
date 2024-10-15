e242e7f82027caaf9dcc36c4f6ccac6c907d61a2
Dledes <davidpewen_101@hotmail.com>
2024-10-15 02:18:44 -0300

    added util to remove deleted items in arrays of objects

--- backend/schema/utils.ts
+++ backend/schema/utils.ts
@@ -24,3 +24,18 @@ export const updateImageSignedUrlAsync = async (image: Image) => {
         },
     });
 };
+
+export function removeDeleted<T extends { deleted: boolean }>(arr: T[]): T[] {
+    return arr
+        .filter((item) => !item.deleted)
+        .map((item) => {
+            // Iterar sobre las propiedades del objeto
+            for (const key in item) {
+                if (Array.isArray(item[key])) {
+                    // Si la propiedad es un arreglo, aplicar la función recursivamente
+                    (item[key] as any) = removeDeleted(item[key]);
+                }
+            }
+            return item;
+        });
+}
