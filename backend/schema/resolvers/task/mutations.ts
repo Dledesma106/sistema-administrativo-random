163432b080706604d0e4efe3f5ec044c1c71e45f
Dledes <davidpewen_101@hotmail.com>
2024-10-18 05:21:17 -0300

    updated updateMyAssignedTask mutation and input type to update the closedAt field on Task

--- backend/schema/resolvers/task/mutations.ts
+++ backend/schema/resolvers/task/mutations.ts
@@ -53,8 +53,9 @@ const UpdateMyTaskInput = builder.inputType('UpdateMyTaskInput', {
     fields: (t) => ({
         id: t.string({ required: true }),
         workOrderNumber: t.string({ required: true }),
-        imageKeys: t.stringList({ required: true }),
+        imageKeys: t.stringList({ required: false }),
         observations: t.string({ required: false }),
+        closedAt: t.field({ type: 'DateTime', required: false }),
     }),
 });
 
@@ -345,7 +346,7 @@ builder.mutationFields((t) => ({
         resolve: async (root, args, { user }) => {
             try {
                 const {
-                    input: { id, workOrderNumber, imageKeys, observations },
+                    input: { id, workOrderNumber, imageKeys, observations, closedAt },
                 } = args;
 
                 const foundTask = await prisma.task.findUniqueUndeleted({
@@ -383,14 +384,17 @@ builder.mutationFields((t) => ({
                         workOrderNumber:
                             parseInt(workOrderNumber, 10) ?? foundTask.workOrderNumber,
                         status: TaskStatus.Finalizada,
+                        closedAt: closedAt,
                         observations,
                         images: {
-                            create: await Promise.all(
-                                imageKeys.map(async (key) => ({
-                                    ...(await createImageSignedUrlAsync(key)),
-                                    key,
-                                })),
-                            ),
+                            create: imageKeys
+                                ? await Promise.all(
+                                      imageKeys.map(async (key) => ({
+                                          ...(await createImageSignedUrlAsync(key)),
+                                          key,
+                                      })),
+                                  )
+                                : [],
                         },
                     },
                 });
